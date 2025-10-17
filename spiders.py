9fdeed03f310dd092dc582b82bd82aee934b2064
César <cesarpichon@gmail.com>
2025-10-17 12:04:07 +0200

    new spiders

---
+++ spiders.py
@@ -0,0 +1,19 @@
+from minet.crawl import Crawler, CrawlTarget
+from ural import format_url
+
+def governo(job, response):
+    next_link = response.soup().scrape_one("li.pager-next > a", "href")
+
+    links = response.soup().scrape("a.box_text_anchor", "href")
+
+    links = [format_url("https://www.governo.it", path=l) for l in links if "/articolo/" in l]
+
+    return links, CrawlTarget(next_link)
+
+def esteri(job, response):
+    next_link = response.soup().scrape_one(".nav-links a.next", "href")
+
+    links = response.soup().scrape(".card-wrapper .big-heading a", "href")
+
+    return links, next_link
+""
